1ae02d1dcf37ded2c2991d94273fa00b9bd59d10
Zara Yousuf Khan <zaara.khan1795.zy@gmail.com>
2024-09-22 04:20:06 +0500

    Add files via upload

---
+++ for-loop02/main.js
@@ -0,0 +1,9 @@
+"use strict";
+// Creating An Array
+let petBirds = ["Parrot", "Macaw", "Cocktail", "Greyparrot"];
+// Using For-Loop
+for (let petBirds_one of petBirds) {
+    console.log(`\n ⁍ A ${petBirds_one} would be a great Pet Bird.`);
+}
+// Printing the message outside the For-Loop 
+console.log("\n 🐦These Birds will produce nice sounds.🐦");
---
+++ for-loop02/main.ts
@@ -0,0 +1,10 @@
+// Creating An Array
+let petBirds = ["Parrot","Macaw","Cocktail","Greyparrot"]
+
+// Using For-Loop
+for (let petBirds_one of petBirds){
+    console.log(`\n ⁍ A ${petBirds_one} would be a great Pet Bird.`)
+}
+
+// Printing the message outside the For-Loop 
+console.log("\n 🐦These Birds will produce nice sounds.🐦")
